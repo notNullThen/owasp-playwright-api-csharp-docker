3585b71cea93497d8dab7316dbbcc2455e5777f3
notNullThen <epekaryuk@outlook.com>
2025-12-30 19:47:41 +0100

    Utils

---
+++ OwaspPlaywrightTests/Support/Utils.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Playwright;
+
+namespace OwaspPlaywrightTests.Support;
+
+public static class Utils
+{
+    public const string PRICE_SYMBOL = "¤";
+
+    public static async Task WaitForElementToBeStable(ILocator element)
+    {
+        var handle = await element.ElementHandleAsync();
+        if (handle != null)
+        {
+            await handle.WaitForElementStateAsync(ElementState.Stable);
+        }
+    }
+
+    public static string FormatPrice(decimal price)
+    {
+        string priceString = price <= 99.99m ? price.ToString("F2") : price.ToString("F0");
+        return $"{priceString}{PRICE_SYMBOL}";
+    }
+
+    public static decimal GetPriceFromText(string priceText)
+    {
+        return decimal.Parse(priceText.Replace(PRICE_SYMBOL, ""));
+    }
+
+    public static async Task DismissCookies(IBrowserContext context)
+    {
+        await context.AddCookiesAsync([
+            new Cookie
+            {
+                Name = "cookieconsent_status",
+                Value = "dismiss",
+                Url = GetBaseUrl(),
+            },
+        ]);
+    }
+
+    public static async Task DismissWelcomeBanner(IBrowserContext context)
+    {
+        await context.AddCookiesAsync([
+            new Cookie
+            {
+                Name = "welcomebanner_status",
+                Value = "dismiss",
+                Url = GetBaseUrl(),
+            },
+        ]);
+    }
+
+    public static string GetBaseUrl()
+    {
+        return !string.IsNullOrEmpty(Environment.GetEnvironmentVariable("CI"))
+            ? "http://juice-shop:3000"
+            : "http://localhost:3000";
+    }
+
+    public static string ConnectUrlParts(params string[] parts)
+    {
+        var connectedParts = string.Join(
+            "/",
+            parts
+                .Where(part => !string.IsNullOrEmpty(part))
+                .Select(NormalizeUrl)
+                .Where(part => part.Trim().Length > 0)
+        );
+
+        return connectedParts + "/";
+    }
+
+    public static string NormalizeUrl(string url)
+    {
+        return RemoveLeadingSlash(RemoveTrailingSlash(url));
+    }
+
+    public static string RemoveTrailingSlash(string url)
+    {
+        return url.EndsWith('/') ? url.Substring(0, url.Length - 1) : url;
+    }
+
+    public static string RemoveLeadingSlash(string url)
+    {
+        return url.StartsWith('/') ? url.Substring(1) : url;
+    }
+}
