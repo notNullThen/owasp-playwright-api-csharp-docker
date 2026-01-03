6fc8ec552accf2dfd0e11bccfc91f4300ac2f555
notNullThen <epekaryuk@outlook.com>
2026-01-03 18:56:32 +0100

    will use fixtures and collections later if needed

--- OwaspPlaywrightTests/Collections/LoggedInCollection.cs
+++
@@ -1,6 +0,0 @@
-using OwaspPlaywrightTests.Fixtures;
-
-namespace OwaspPlaywrightTests.Collections;
-
-[CollectionDefinition("Logged in collection")]
-public class LoggedInCollection : ICollectionFixture<PrepareRandomUserFixture> { }
