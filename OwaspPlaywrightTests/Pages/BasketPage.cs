0d0d4b5eaa193556b100d2076cd3bb10a51c93e1
notNullThen <epekaryuk@outlook.com>
2026-01-04 18:05:38 +0100

    BasketPage GotoAsync() adjusted with API call

--- OwaspPlaywrightTests/Pages/BasketPage.cs
+++ OwaspPlaywrightTests/Pages/BasketPage.cs
@@ -1,3 +1,6 @@
+using OwaspPlaywrightTests.ApiEndpoints;
+using OwaspPlaywrightTests.ApiEndpoints.Types.RestBasket;
+using OwaspPlaywrightTests.Base.ApiHandler.Types;
 using OwaspPlaywrightTests.Components;
 
 namespace OwaspPlaywrightTests.Pages;
@@ -5,4 +8,13 @@ namespace OwaspPlaywrightTests.Pages;
 public class BasketPage() : PageBase("#/basket")
 {
     public ProductRow Products => new();
+
+    public new async Task<BrowserApiResponse<RestBasketResponse>> GotoAsync()
+    {
+        var basketWaitTask = Api.RestBasket.GetBasket().WaitAsync();
+
+        await Task.WhenAll(base.GotoAsync(), basketWaitTask);
+
+        return await basketWaitTask;
+    }
 }
