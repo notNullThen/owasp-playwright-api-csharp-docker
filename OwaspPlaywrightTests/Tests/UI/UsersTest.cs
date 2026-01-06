a2cee44871ab1a59076c2f27de1bed98511af791
notNullThen <epekaryuk@outlook.com>
2026-01-06 17:40:52 +0100

    naming for users test

--- OwaspPlaywrightTests/Tests/UI/UsersTest.cs
+++ OwaspPlaywrightTests/Tests/UI/UsersTest.cs
@@ -6,7 +6,7 @@ using Xunit.Abstractions;
 
 namespace OwaspPlaywrightTests.Tests.UI;
 
-public class AuthorizationTest
+public class UsersTest
 {
     public class RegistrationTests(ITestOutputHelper outputHelper) : Test(outputHelper)
     {
