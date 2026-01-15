7203c994897c3e4312411327ff5c46ad5ec1ae63
notNullThen <epekaryuk@outlook.com>
2026-01-15 17:13:52 +0100

    locking InitialConfig and BaseUrl ready

--- OwaspPlaywrightTests/GlobalSetup.cs
+++ OwaspPlaywrightTests/GlobalSetup.cs
@@ -5,17 +5,36 @@ namespace OwaspPlaywrightTests;
 
 public static class GlobalSetup
 {
+    private static bool _initialized;
+    private static readonly SemaphoreSlim _semaphore = new(1, 1);
+
     public static async Task GlobalSetupAsync()
     {
-        ApiParametersBase.SetInitialConfig(
-            apiWaitTimeout: TestConfig.ApiWaitTimeout,
-            expectedStatusCodes: TestConfig.ExpectedApiStatusCodes,
-            baseUrl: TestConfig.BaseUrl
-        );
+        if (!_initialized)
+        {
+            await _semaphore.WaitAsync();
+            try
+            {
+                if (!_initialized)
+                {
+                    ApiParametersBase.SetInitialConfig(
+                        apiWaitTimeout: TestConfig.ApiWaitTimeout,
+                        expectedStatusCodes: TestConfig.ExpectedApiStatusCodes,
+                        baseUrl: TestConfig.BaseUrl
+                    );
+
+                    await TestUtils.WaitForBaseUrlReadyAsync();
+
+                    _initialized = true;
+                }
+            }
+            finally
+            {
+                _semaphore.Release();
+            }
+        }
 
         await TestUtils.DismissCookiesAsync();
         await TestUtils.DismissWelcomeBannerAsync();
-
-        await TestUtils.WaitForBaseUrlReadyAsync();
     }
 }
