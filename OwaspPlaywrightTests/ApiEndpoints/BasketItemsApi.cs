d889846da29b8f1d041072ec1264e737d83616dc
notNullThen <epekaryuk@outlook.com>
2026-03-09 19:14:13 +0100

    added example method

--- OwaspPlaywrightTests/ApiEndpoints/BasketItemsApi.cs
+++ OwaspPlaywrightTests/ApiEndpoints/BasketItemsApi.cs
@@ -8,4 +8,7 @@ public class BasketItemsApi() : ApiEndpointBase("api/BasketItems")
 {
     public ApiAction<BasketItemsResponse> PostBasketItems(BasketItemsPayload? payload = null) =>
         Action<BasketItemsResponse>(new() { Method = ApiHttpMethod.POST, Body = payload });
+
+    public ApiAction<object> DeleteBasketItems(string? userId = null) =>
+        Action<object>(new() { Method = ApiHttpMethod.DELETE, Url = userId });
 }
