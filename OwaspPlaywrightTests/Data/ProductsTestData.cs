f726c1996aaef2b9ce8bdd269ca6ea42f99f6ef7
notNullThen <epekaryuk@outlook.com>
2026-03-28 12:55:53 +0100

    better products data creation

--- OwaspPlaywrightTests/Data/ProductsTestData.cs
+++ OwaspPlaywrightTests/Data/ProductsTestData.cs
@@ -4,17 +4,18 @@ public sealed record Product(string Name, string Description, float Price, bool 
 
 public static class ProductsTestData
 {
-    public static readonly Product RaspberryJuice = new(
-        Name: "Raspberry Juice (1000ml)",
-        Description: "Made from blended Raspberry Pi, water and sugar.",
-        Price: 4.99f,
-        SoldOut: false
-    );
-
-    public static readonly Product BananaJuice = new(
-        Name: "Banana Juice (1000ml)",
-        Description: "Monkeys love it the most.",
-        Price: 1.99f,
-        SoldOut: true
-    );
+    public static readonly Product RaspberryJuice =
+            new(
+                Name: "Raspberry Juice (1000ml)",
+                Description: "Made from blended Raspberry Pi, water and sugar.",
+                Price: 4.99f,
+                SoldOut: false
+            ),
+        BananaJuice =
+            new(
+                Name: "Banana Juice (1000ml)",
+                Description: "Monkeys love it the most.",
+                Price: 1.99f,
+                SoldOut: true
+            );
 }
