87c5b398b9cd822cdbc95def8005839c8f897069
notNullThen <epekaryuk@outlook.com>
2026-04-14 22:38:10 +0200

    ApiClient.SetContext

--- OwaspPlaywrightTests/ApiEndpoints/Api.cs
+++ OwaspPlaywrightTests/ApiEndpoints/Api.cs
@@ -1,19 +1,20 @@
+using OwaspPlaywrightTests.Base;
+using SimpleApiPlaywright;
+
 namespace OwaspPlaywrightTests.ApiEndpoints;
 
 public static class Api
 {
-    private static readonly AsyncLocal<SecurityAnswersApi> _securityAnswers = new();
-    private static readonly AsyncLocal<RestUserApi> _restUser = new();
-    private static readonly AsyncLocal<UsersApi> _users = new();
-    private static readonly AsyncLocal<BasketItemsApi> _basketItems = new();
-    private static readonly AsyncLocal<RestBasketApi> _restBasket = new();
-    private static readonly AsyncLocal<ProductsApi> _products = new();
+    private static T Create<T>(T instance)
+    {
+        ApiClient.SetContext(new ApiContext(Test.Page.Context, Test.Page, Test.Page.APIRequest));
+        return instance;
+    }
 
-    public static SecurityAnswersApi SecurityAnswers =>
-        _securityAnswers.Value ??= new SecurityAnswersApi();
-    public static RestUserApi RestUser => _restUser.Value ??= new RestUserApi();
-    public static UsersApi Users => _users.Value ??= new UsersApi();
-    public static BasketItemsApi BasketItems => _basketItems.Value ??= new BasketItemsApi();
-    public static ProductsApi Products => _products.Value ??= new ProductsApi();
-    public static RestBasketApi RestBasket => _restBasket.Value ??= new RestBasketApi();
+    public static SecurityAnswersApi SecurityAnswers => Create(new SecurityAnswersApi());
+    public static RestUserApi RestUser => Create(new RestUserApi());
+    public static UsersApi Users => Create(new UsersApi());
+    public static BasketItemsApi BasketItems => Create(new BasketItemsApi());
+    public static ProductsApi Products => Create(new ProductsApi());
+    public static RestBasketApi RestBasket => Create(new RestBasketApi());
 }
